4a035f51b46d42586af5ae1c55f197d7c321e289
Chase Anderson <chaseanderson.stu@gmail.com>
2023-11-08 11:47:17 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solution here
+function findMatching(drivers, string) {
+    const lowercaseString = string.toLowerCase();
+    const matchingDrivers = drivers.filter(driver =>
+      driver.toLowerCase() === lowercaseString
+    );
+    return matchingDrivers;
+  }
+
+function fuzzyMatch(drivers, string) {
+    const lowercaseString = string.toLowerCase();
+    const matchingDrivers = drivers.filter(driver =>
+      driver.toLowerCase().startsWith(lowercaseString)
+    );
+    return matchingDrivers;
+  }
+
+  function matchName(drivers, string) {
+    const matchingDrivers = drivers.filter(driver =>
+      driver.name === string
+    );
+    return matchingDrivers;
+  }
